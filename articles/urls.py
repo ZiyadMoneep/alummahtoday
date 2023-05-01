a4a560af4ca9ea678d4284cdd02eb66ea8aceccb
Gamal <gamalmoneep@gmail.com>
2023-05-01 12:51:17 +0300

    add dev tools

--- articles/urls.py
+++ articles/urls.py
@@ -19,14 +19,14 @@ from .views import ArticleListView, ArticleCreateView, ArticleDeleteView, Articl
 app_name = 'articles'
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list'),
-
-    path('create', ArticleCreateView.as_view(), name='article_create'),
-
-    path('<int:pk>/delete', ArticleDeleteView.as_view(), name='article_delete'),
-
-    path('<int:pk>/detail', ArticleDetailView.as_view(), name='article_detail'),
-
-    path('<int:pk>/update', ArticleUpdateView.as_view(), name='article_update'),
+    # path('', ArticleListView.as_view(), name='article_list'),
+    #
+    # path('create', ArticleCreateView.as_view(), name='article_create'),
+    #
+    # path('<int:pk>/delete', ArticleDeleteView.as_view(), name='article_delete'),
+    #
+    # path('<int:pk>/detail', ArticleDetailView.as_view(), name='article_detail'),
+    #
+    # path('<int:pk>/update', ArticleUpdateView.as_view(), name='article_update'),
 
 ]
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('articles.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                      path("__reload__/", include("django_browser_reload.urls")),
+                  ] + urlpatterns
